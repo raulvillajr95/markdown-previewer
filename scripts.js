58fdeeb82b7d3b4681ecd31a3752c0e05e7741a1
Raul <raulvillajr95@gmail.com>
2022-04-18 17:36:17 -0700

    Introduce React to project

---
+++ scripts.js
@@ -0,0 +1,12 @@
+import * as React from "https://cdn.skypack.dev/react@17.0.1";
+
+const JSX = (
+  <div>
+    <h1>This is a block of JSX</h1>
+    <p>Here's a subtitle</p>
+    {/* sup */}
+    <p>Here's a subtitle</p>
+  </div>
+);
+
+ReactDOM.render(JSX, document.getElementById('preview'))
