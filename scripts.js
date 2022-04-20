86f5cc38ad1f0257725f4595cee276c5c1abe9f2
Raul <raulvillajr95@gmail.com>
2022-04-19 22:05:03 -0700

    Add some flex to 'Markdown Previewer' project from FCC

--- scripts.js
+++ scripts.js
@@ -10,6 +10,8 @@ class ControlledInput extends React.Component {
     super(props);
     this.state = {
       input: `
+![Keyboard](https://contact-centres.com/wp-content/uploads/2017/06/tech.image_.june_.2017.jpg)
+      
 # JavaScript Program to Print Hello World
 In this example, you will learn to print 'Hello World' in JavaScript in three
 different ways.
@@ -49,19 +51,28 @@ prints the \`Hello, World!\` string to the console.
 ---
 
 ## 2. Using alert()
+The \`alert()\` method displays an alert box over the current window with the
+specified message.
 
+**Source Code**
+\`\`\`
+// the hello world program
+alert("Hello, World!");
+\`\`\`
 
+---
 
+## 3. Using document.write()
+\`document.write()\` is used when you want to print the content to the HTML
+document.
 
+**Source Code**
+\`\`\`
+// the hello world program
+alert("Hello, World!");
+\`\`\`
 
-
-[GitHub](https://github.com/)
-\`(condtion) ? true : false\`
-- one
-> foo
-![cat](https://icatcare.org/app/uploads/2019/09/The-Kitten-Checklist-1.png)
-**BOLD**
-Courtesy of &copy;Programiz
+> Courtesy of &copy;[Programiz](https://www.programiz.com/javascript/examples/hello-world)
 `
     };
     this.handleChange = this.handleChange.bind(this);
@@ -75,10 +86,27 @@ Courtesy of &copy;Programiz
     let content = marked.parse(this.state.input);
     let defaultVal = "Header";
     return (
-      <div>
-        <textarea id="editor" value={this.state.input} onChange={this.handleChange}></textarea>
-        <h4>Controlled Input:</h4>
-        <div id="preview" className="Container" dangerouslySetInnerHTML={{__html: content}}></div>
+      <div class="d-flex flex-column justify-content-center align-items-center">
+        <div class="d-flex flex-column container-sm">
+          <div class="d-flex justify-content-between align-items-center">
+            <div class="d-flex align-items-center">
+              <i class="fa-solid fa-fire"></i>
+              <h4>Editor</h4>
+            </div>
+            <a><i class="fa-solid fa-xmark fa-xl"></i></a>
+         </div>
+          <textarea id="editor" value={this.state.input} onChange={this.handleChange}></textarea>
+        </div>
+          <div class="d-flex flex-column container-md">
+            <div class="d-flex justify-content-between align-items-center">
+              <div class="d-flex align-items-center">
+                <i class="fa-solid fa-fire"></i>
+                <h4>Previewer</h4>
+              </div>
+              <a><i class="fa-solid fa-xmark fa-xl"></i></a>
+            </div>
+            <div id="preview" className="Container" dangerouslySetInnerHTML={{__html: content}}></div>
+          </div>
       </div>
     );
   }
